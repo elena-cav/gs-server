24be9812b2cf495270681bb1af060cb5dedf27bc
elena-cav <elenacavallero@outlook.com>
2021-07-03 13:05:09 +0100

    add listen

---
+++ listen.js
@@ -0,0 +1,8 @@
+const app = require('./app');
+
+const PORT = process.env.PORT || 9090;
+
+app.listen(PORT, (err) => {
+  if (err) throw err;
+  console.log(`Server listening on port ${PORT}...`);
+});
